5ebf6aee1e9834646e1151e661c0c08ca80ae2f1
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2018-02-01 16:46:19 -0200

    Update database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,11 @@
 <?php
 
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));		
+    $host = $url["host"];		
+    $username = $url["user"];		
+    $password = $url["pass"];		
+    $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -30,11 +36,6 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));		
-    $host = $url["host"];		
-    $username = $url["user"];		
-    $password = $url["pass"];		
-    $database = substr($url["path"], 1);
 
     'connections' => [
 
