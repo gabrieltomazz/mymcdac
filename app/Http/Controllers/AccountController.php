5ca9dea0a3f035b0ff8566b1f1330a8ed9db8f70
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2018-02-14 22:18:53 -0200

    Update AccountController

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,21 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use App\User;
-
-use Carbon\Carbon;
 use App\Http\Requests;
 
-use App\Exceptions\BusinessException;
-
-use Illuminate\Support\Facades\DB;
 
 class AccountController extends Controller
 {
-    
-    use RegistersUsers;
 
     public function index(){
         return view('account');
@@ -30,10 +21,7 @@ class AccountController extends Controller
         $users->fill($request->all());
         $users->save();
        
-
         return $users;
-
-
 
     }
 
